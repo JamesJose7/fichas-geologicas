fee1ef47eb5e28762fd1d90307b4b28e06dd8022
Jose Eduardo Eguiguren <pepe.96.jose@gmail.com>
2019-01-13 02:46:24 -0500

    Deleting and loading data each time the server runs (test only)

--- src/main/java/com/arqapps/core/DatabaseLoader.java
+++ src/main/java/com/arqapps/core/DatabaseLoader.java
@@ -1,5 +1,8 @@
 package com.arqapps.core;
 
+import com.arqapps.estructura_geologica.Dique;
+import com.arqapps.estructura_geologica.EstructuraGeologicaRepository;
+import com.arqapps.estructura_geologica.Foliacion;
 import com.arqapps.ficha_campo.FichaCampo;
 import com.arqapps.ficha_campo.FichaCampoRepository;
 import com.arqapps.ficha_campo.Ubicacion;
@@ -16,18 +19,49 @@ public class DatabaseLoader implements ApplicationRunner {
     private final FichaCampoRepository fichas;
     private final UbicacionRepository ubicaciones;
 
+    private final EstructuraGeologicaRepository mEstructuraGeologicaRepository;
+
     @Autowired
-    public DatabaseLoader(FichaCampoRepository fichas, UbicacionRepository ubicaciones) {
+    public DatabaseLoader(FichaCampoRepository fichas, UbicacionRepository ubicaciones,
+                          EstructuraGeologicaRepository estructuraGeologicaRepository) {
         this.fichas = fichas;
         this.ubicaciones = ubicaciones;
+        mEstructuraGeologicaRepository = estructuraGeologicaRepository;
     }
 
     @Override
     public void run(ApplicationArguments args) {
+        // Delete previous data !!!
+        fichas.deleteAll();
+        ubicaciones.deleteAll();
+        mEstructuraGeologicaRepository.deleteAll();
+
+        // Ficha de campo
         FichaCampo ficha = new FichaCampo("Muestra", "origen", "unidad", "contacto");
+        FichaCampo ficha2 = new FichaCampo("Muestra", "origen", "unidad", "contacto");
+
+        // Ubicacion de ficha de campo
         Ubicacion ubicacion = new Ubicacion(new Date(), "sdf", "sdf", "fsd", "fsdf");
         ubicaciones.save(ubicacion);
+        Ubicacion ubicacion2 = new Ubicacion(new Date(), "sdf", "sdf", "fsd", "fsdf");
+        ubicaciones.save(ubicacion2);
+
+        // Estructuras geologicas
+        Dique dique = new Dique("categoria", "dique", "description", "institucion",
+                "clase", "textura");
+        mEstructuraGeologicaRepository.save(dique);
+        Foliacion foliacion = new Foliacion("categoria", "foliacion", "description", "institucion",
+                "metaforicas", "igneas");
+        mEstructuraGeologicaRepository.save(foliacion);
+
+        // Save ficha de campo
         ficha.setUbicacion(ubicacion);
+        ficha.setEstructuraGeologica(dique);
         fichas.save(ficha);
+        ficha2.setUbicacion(ubicacion2);
+        ficha2.setEstructuraGeologica(foliacion);
+        fichas.save(ficha2);
+
+
     }
 }
