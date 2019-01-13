e492ef21a454d3179fbd901791e7dd2bceb88eac
Jose Eduardo Eguiguren <pepe.96.jose@gmail.com>
2019-01-13 00:03:23 -0500

    Changed default url and reference name for ficha de campo

--- src/main/java/com/arqapps/ficha_campo/FichaCampoRepository.java
+++ src/main/java/com/arqapps/ficha_campo/FichaCampoRepository.java
@@ -1,8 +1,8 @@
 package com.arqapps.ficha_campo;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RestResource;
 
-@Repository
+@RestResource(path = "fichasCampo", rel = "fichasCampo")
 public interface FichaCampoRepository extends CrudRepository<FichaCampo, Long> {
 }
