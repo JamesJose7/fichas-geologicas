78b5ad18af4a5624f7e3ced5122bc77342e3c124
Jose Eduardo Eguiguren <pepe.96.jose@gmail.com>
2019-01-12 22:55:11 -0500

    Added mysql as the datasource for the api

--- src/main/java/com/arqapps/Application.java
+++ src/main/java/com/arqapps/Application.java
@@ -1,4 +1,11 @@
 package com.arqapps;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
