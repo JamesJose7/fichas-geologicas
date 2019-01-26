f9dda179514dd0a0f91d647fd4ab9e72a8c8bcd2
Jose Eduardo Eguiguren <pepe.96.jose@gmail.com>
2019-01-26 00:41:40 -0500

    Forgot heroku is not compatible with java 11

--- src/main/java/com/arqapps/core/DatabaseLoader.java
+++ src/main/java/com/arqapps/core/DatabaseLoader.java
@@ -244,12 +244,12 @@ public class DatabaseLoader implements ApplicationRunner {
     }
 
     private String getLorem(int repeats) {
-        String lorem = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\n" +
+        return  "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\n" +
                 "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\n" +
                 "quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\n" +
                 "consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\n" +
                 "cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\n" +
                 "proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ";
-        return lorem.repeat(repeats);
+//        return lorem.repeat(repeats);
     }
 }
