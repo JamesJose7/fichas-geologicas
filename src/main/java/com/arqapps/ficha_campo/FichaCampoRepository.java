a37225df8535fe5c690e81ee77f1e92cd07cc57b
Jose Eduardo Eguiguren <pepe.96.jose@gmail.com>
2019-02-03 14:45:44 -0500

    Added event handler to save fichas with the logged in user and prevent deletion from other users

--- src/main/java/com/arqapps/ficha_campo/FichaCampoRepository.java
+++ src/main/java/com/arqapps/ficha_campo/FichaCampoRepository.java
@@ -1,6 +1,7 @@
 package com.arqapps.ficha_campo;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.security.access.prepost.PostFilter;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -16,4 +17,12 @@ public interface FichaCampoRepository extends CrudRepository<FichaCampo, Long> {
     @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
     @PostFilter("filterObject.user.username == authentication.name or hasRole('ROLE_ADMIN')")
     Iterable<FichaCampo> findAll(Iterable<Long> longs);
+
+    @Override
+    @PreAuthorize("hasRole('ROLE_ADMIN') or @fichaCampoRepository.findOne(#id)?.user?.username == authentication.name")
+    void delete(@Param("id") Long id);
+
+    @Override
+    @PreAuthorize("hasRole('ROLE_ADMIN') or #fichaCampo.user?.username == authentication.name")
+    void delete(@Param("fichaCampo") FichaCampo fichaCampo);
 }
